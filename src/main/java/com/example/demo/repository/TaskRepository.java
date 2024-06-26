5f200009ffb39f5b2ef64d81d9770838cabb96dd
Abraham <abraham.olague@gmail.com>
2024-06-25 22:00:53 -0600

    added UUid

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -1,7 +1,9 @@
+// repository/TaskRepository.java
 package com.example.demo.repository;
 
 import com.example.demo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, UUID> {
 }
